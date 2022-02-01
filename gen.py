0ad890dff0c312d2281d7a3302953d3cc9b5b6e5
catsolot <milocraun@gmail.com>
2022-02-01 18:45:36 -0500

    Add files via upload

---
+++ gen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+tag = "\item"
+sep = "&"
+line = "\hline"
+end = "\\\\"
+
+def gen(n):
+    print("\\begin{center}")
+    print("\\begin{tabular}{| c | c |}")
+    print("\\hline")
+    for i in range (65, 65+26):
+        alpha = chr(i)
+        if i + n <= 90:
+            letter = chr(i+n)
+        else:
+            letter = chr((i + n) - 26)
+        print(alpha, sep, letter, end, end = " ")
+        print("\hline", end = " ")
+    print("\\end{tabular}")
+    print("\\end{center}")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        print("Usage: ./gen.py n")
+        print("Where n is the caesar cipher key")
+        exit()
+    key = int(sys.argv[len(sys.argv) - 1])
+    gen(key)
