419a4e7770b8a82684ee51cae9264f181ea3deb5
milo craun <milo@ipsum.localdomain>
2022-02-02 12:18:46 -0500

    Reorganized.

---
+++ scripts/gen.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+
+sep = "&"
+line = "\hline"
+end = "\\\\"
+
+def gen(n):
+    print("\\begin{center}")
+    print("\\begin{tabular}{| c | c |}")
+    print("\\hline Plaintext & Ciphertext \\\\ \\hline", end = " ")
+    for i in range (65, 65+26):
+        alpha = chr(i)
+        if i + n <= 90:
+            letter = chr(i+n)
+        else:
+            letter = chr((i + n) - 26)
+        print(alpha, sep, letter, end, line, end = " ")
+    print("\\end{tabular}")
+    print("\\end{center}")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2 or len(sys.argv) <= 1:
+        print("Usage: ./gen.py n")
+        print("Where n is the caesar cipher key")
+        exit()
+    key = int(sys.argv[len(sys.argv) - 1])
+    gen(key)
